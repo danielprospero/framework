298b285afbd3e59eaa6b46a91de5964a89a48ab6
Daniel Próspero Ribeiro <daniel.ribeiro@elev.tec.br>
2023-10-04 12:57:35 -0300

    Configurando o autoload.php e Conexão.php

---
+++ app/Views/rodape.php
@@ -0,0 +1,12 @@
+<footer class="p-5">
+
+    <div class="d-flex flex-column flex-sm-row  justify-content-between py-4 my-4 border-top">
+        <p>&copy; 2023 - <?=date('Y')?> Todos direitos reservados a Framwork.</p>
+        <ul class="list-unstyled d-flex">    
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-twitter"></i></a></li>
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-instagram"></i></a></li>
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-facebook"></i></a></li>
+        </ul>
+    </div>
+
+</footer>
